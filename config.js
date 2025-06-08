aad9d8e73aca040a6d3abe8fedb856c555237b78
Musical-Instrument-PC <kjeffreyleung@gmail.com>
2025-06-07 23:13:59 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ENV = {
-  SUPABASE_URL: "{{ SUPABASE_URL }}",
-  SUPABASE_ANON_KEY: "{{ SUPABASE_ANON_KEY }}"
+  SUPABASE_URL: "https://ecdymetkawuatkfmmiow.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjZHltZXRrYXd1YXRrZm1taW93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNTcxMTUsImV4cCI6MjA2NDkzMzExNX0.osrxz2y3upefFR0mXvhjqGkj9pVlQbLMbb0WG2yv8h0"
 };
