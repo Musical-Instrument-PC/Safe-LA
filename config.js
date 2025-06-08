c44bd52e81735d80798033496e8a89de83784970
Musical-Instrument-PC <kjeffreyleung@gmail.com>
2025-06-07 23:22:45 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.ENV = {
-  SUPABASE_URL: "https://ecdymetkawuatkfmmiow.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjZHltZXRrYXd1YXRrZm1taW93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNTcxMTUsImV4cCI6MjA2NDkzMzExNX0.osrxz2y3upefFR0mXvhjqGkj9pVlQbLMbb0WG2yv8h0"
-};
